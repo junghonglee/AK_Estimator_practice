71116df3bcfbc37aaf4deca8a46da0ee25f9b784
JHL <junghonglee@yonsei.ac.kr>
2018-01-31 18:20:02 +0900

    Simulation 2000

--- 0131_3000_bootstrap_combined.R
+++ 0131_3000_bootstrap_combined.R
@@ -1,16 +1,17 @@
 
-a <- read.csv(file = "C:\\Users\\JHL\\Desktop\\thesis\\Local vs Global\\0125.csv", header=T)
-b <- read.csv(file = "C:\\Users\\JHL\\Desktop\\thesis\\Local vs Global\\0130.csv", header=T)
-c <- read.csv(file = "C:\\Users\\JHL\\Desktop\\thesis\\Local vs Global\\0131.csv", header=T)
+a <- read.csv(file = "C:\\Users\\JHL\\Desktop\\thesis\\Local vs Global\\0130.csv", header=T)
+b <- read.csv(file = "C:\\Users\\JHL\\Desktop\\thesis\\Local vs Global\\0131.csv", header=T)
 
-simul_3000 <- rbind( a,b,c)
+simul_2000 <- rbind(a,b)
 
-nrow(simul_3000)
+ncol(simul_2000)
+nrow(simul_2000)
 
 boot_result <- matrix(0,1,4)
 colnames(boot_result) <- (c("ak2","ak3","ak4","ak5"))
 
-boot_result[1] <- mean(sqrt(simul_3000[[5]])) / sd(simul_3000[[1]])
-boot_result[2] <- mean(sqrt(simul_3000[[6]])) / sd(simul_3000[[2]])
-boot_result[3] <- mean(sqrt(simul_3000[[7]])) / sd(simul_3000[[3]])
-boot_result[4] <- mean(sqrt(simul_3000[[8]])) / sd(simul_3000[[4]])
+boot_result[1] <- mean(sqrt(simul_2000[[6]])) / sd(simul_2000[[2]])
+boot_result[2] <- mean(sqrt(simul_2000[[7]])) / sd(simul_2000[[3]])
+boot_result[3] <- mean(sqrt(simul_2000[[8]])) / sd(simul_2000[[4]])
+boot_result[4] <- mean(sqrt(simul_2000[[9]])) / sd(simul_2000[[5]])
+boot_result
