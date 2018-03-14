7f223bdd13aeaee03b5439692f89d2b059770f99
JHL <junghonglee@yonsei.ac.kr>
2018-03-14 17:18:15 +0900

    Update

--- AK_ESTIMATOR_GLOBAL.R
+++ AK_ESTIMATOR_GLOBAL.R
@@ -271,16 +271,16 @@ result_show <- function(simul, boot_simul, time_elap, SRS_var, SRS_mean, global_
   cat("\n\n")
   
   cat("# Variance Performance\n")
-  cat("simple / global", mean(sqrt(SRS_var)) / mean(sqrt(global_var)), "\n")
+  cat("simple / global = ", mean(sqrt(SRS_var)) / mean(sqrt(global_var)), "\n")
   cat("\n\n")
   
   cat("# Mean Performance \n")
-  cat("Population / SRS", popul[t]/100 / mean(SRS_mean), "\n")
-  cat("Population / Global", popul[t]/100 / mean(global_mean), "\n")
+  cat("Population / SRS = ", popul[t]/100 / mean(SRS_mean), "\n")
+  cat("Population / Global = ", popul[t]/100 / mean(global_mean), "\n")
   cat("\n")
   
   cat("#Bootstrap Test\n")
-  cat("Bootstrap test", mean(sqrt(global_var)) / sd(global_mean), "\n")
+  cat("Bootstrap test = ", mean(sqrt(global_var)) / sd(global_mean), "\n")
 }
 result_show(simul, boot_simul, time_elap, SRS_var, SRS_mean, global_var, global_mean, time_calc)
 
